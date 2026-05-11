e568e02d10fb408dc7ad1910cdba6f877c28624e
Samuel Just <samjust@redpanda.com>
2026-05-11 09:14:23 -0700

    ct/l1: extend fake_io with imported TS segment fixture support
    Extract tiered_storage_object_reader into ts_reader.h/ts_reader.cc so
    that fake_io can construct TS readers without pulling in file_io's
    cloud-storage dependencies. Add put_ts_segment() to fake_io and wire
    the imported branch of open_object to a trivial fake_ts_index (always
    seeks to position 0, correct for test use) and fake_ts_object_handle.

--- src/v/cloud_topics/level_one/common/fake_io.h
+++ src/v/cloud_topics/level_one/common/fake_io.h
@@ -51,8 +51,26 @@ public:
     // Return a list of the object IDs that haven't been removed.
     chunked_vector<object_id> list_objects() const;
 
+    /// Inject a raw TS-format segment for use with open_object on imported
+    /// extents whose ts_path matches. open_object always seeks to
+    /// file_position=0, performing a conservative full-segment scan.
+    void put_ts_segment(
+      ss::sstring ts_path,
+      iobuf segment_bytes,
+      kafka::offset base_kafka_offset,
+      kafka::offset last_kafka_offset,
+      model::offset_delta delta_offset);
+
 private:
+    struct ts_segment_fixture {
+        iobuf bytes;
+        kafka::offset base_kafka_offset;
+        kafka::offset last_kafka_offset;
+        model::offset_delta delta_offset;
+    };
+
     absl::btree_map<object_id, iobuf> _storage;
+    absl::btree_map<ss::sstring, ts_segment_fixture> _ts_storage;
 };
 
 } // namespace cloud_topics::l1
---
+++ src/v/cloud_topics/level_one/common/ts_reader.cc
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+
+#include "cloud_topics/level_one/common/ts_reader.h"
+
+#include "bytes/iostream.h"
+#include "model/record_batch_types.h"
+#include "storage/record_batch_utils.h"
+
+#include <fmt/core.h>
+
+#include <stdexcept>
+
+namespace cloud_topics::l1 {
+
+tiered_storage_object_reader::tiered_storage_object_reader(
+  ss::input_stream<char> stream, model::offset_delta delta)
+  : _stream(std::move(stream))
+  , _running_delta(delta) {}
+
+ss::future<> tiered_storage_object_reader::close() {
+    return _stream.close();
+}
+
+ss::future<object_reader::peek_result> tiered_storage_object_reader::peek() {
+    if (!_peeked.has_value() && !_eof) {
+        auto next = co_await fetch_next_translated();
+        if (next.has_value()) {
+            _peeked = std::move(*next);
+        } else {
+            _eof = true;
+        }
+    }
+    if (_eof) {
+        co_return eof{};
+    }
+    co_return _peeked->header();
+}
+
+ss::future<object_reader::result> tiered_storage_object_reader::read_next() {
+    if (_peeked.has_value()) {
+        auto batch = std::move(*_peeked);
+        _peeked.reset();
+        co_return std::move(batch);
+    }
+    if (_eof) {
+        co_return eof{};
+    }
+    auto next = co_await fetch_next_translated();
+    if (!next.has_value()) {
+        co_return eof{};
+    }
+    co_return std::move(*next);
+}
+
+ss::future<std::optional<model::record_batch>>
+tiered_storage_object_reader::fetch_next_translated() {
+    static const auto translator_types = model::offset_translator_batch_types();
+    for (;;) {
+        auto header_buf = co_await read_iobuf_exactly(
+          _stream, model::packed_record_batch_header_size);
+        if (
+          header_buf.size_bytes() < model::packed_record_batch_header_size) {
+            co_return std::nullopt;
+        }
+        auto header = storage::batch_header_from_disk_iobuf(
+          std::move(header_buf));
+        auto records_size = static_cast<size_t>(header.size_bytes)
+                            - model::packed_record_batch_header_size;
+        auto records_buf = co_await read_iobuf_exactly(_stream, records_size);
+        if (records_buf.size_bytes() != records_size) {
+            throw std::runtime_error(fmt::format(
+              "truncated TS segment: expected {} record bytes, got {}",
+              records_size,
+              records_buf.size_bytes()));
+        }
+        if (std::ranges::contains(translator_types, header.type)) {
+            _running_delta += static_cast<int64_t>(header.record_count);
+            continue;
+        }
+        header.base_offset = model::offset{
+          header.base_offset() - _running_delta()};
+        co_return model::record_batch(
+          header,
+          std::move(records_buf),
+          model::record_batch::tag_ctor_ng{});
+    }
+}
+
+} // namespace cloud_topics::l1
